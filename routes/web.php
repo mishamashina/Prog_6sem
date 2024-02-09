2032d3d209df9e111acd1cacba86ae3265dff8a9
mishamashina <misha_sidorenko228@mail.ru>
2024-02-09 17:35:47 +0300

    Commit 2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\MainController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout');
 });
+
+Route::get('/contacts', function(){
+    $contacts = [
+        'univer' => 'Polytech',
+        'phone' => '8(495)232-2232',
+        'email' => 'mospolytech@mospolytech.ru'
+    ];
+    return view('main.contact', ['contacts'=>$contacts]);
+});
+
+Route::get('/articles', [MainController::class, 'index']);
+Route::get('/full-img/{img}', [MainController::class, 'show']);
